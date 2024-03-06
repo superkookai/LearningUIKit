224293452461133cee8c017ae0e1c07b6e2eb690
Weerawut Chaiyasomboon <weerawut@live.com>
2024-03-06 09:27:03 +0700

    Initial Commit

---
+++ LearningUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearningUIKit
+//
+//  Created by Weerawut Chaiyasomboon on 6/3/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
